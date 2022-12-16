e1d88e879fa8882e970e8668dc342d6fec23207f
Becky Yu <beckymark958@gmail.com>
2022-12-15 23:03:51 -0500

    Update combiner file

--- Scraping(individuals)/Scraping_script_ChemBK.py
+++ Scraping(individuals)/Scraping_script_ChemBK.py
@@ -1,3 +1,10 @@
+"""
+Scraping script for obtaining information from ChemBK.
+Source link: https://www.chembk.com/en
+
+@author: Becky Yu
+"""
+
 # -- Import libraries --
 from cmath import e
 import pandas as pd
@@ -7,93 +14,101 @@ import requests
 from bs4 import BeautifulSoup
 import urllib.parse
 
+def scrapChemBK(header, df, outputFileName):
+    # Write to file
+    with open(outputFileName, 'w', encoding='UTF8', newline="") as f:
+        try:
+            writer = csv.writer(f)
+            writer.writerow(header)
 
-# Read CSV file into dataframe
-df = pd.read_excel("Property_and_functionalities_List.xlsx", header=0)
-print(len(df))
+            count_e = 0  # error count
+            for index, row in df.iterrows():
+                synonyms = ""
+                formula = ""
+                weight = ""
+                density = ""
+                water_solubility = ""
 
-# Drop duplicate chemical names if needed
-# df = df.drop_duplicates(subset='Chemical Name', keep="last")
-# print(len(df))
-# df = df.iloc[:1600]
+                chemical_name = row['Chemical Name'].title().replace("/", " ")
+                
+                headers = {
+                    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:107.0) Gecko/20100101 Firefox/107.0',
+                }
+                url = "www.chembk.com/en/chem/" + chemical_name
 
-# Set file header
-header = ['id', 'Chemical Name', 'LogP (Partition coefficient)', 'LogS (water solubility of the ingredient)',
-          'Molecular weight', 'Density', 'Chemical formula', 'Structure', 'Synonyms', 'Functionalities']
 
-# Write to file
-with open('Properties_Chembk_new.csv', 'w', encoding='UTF8', newline="") as f:
-    try:
-        writer = csv.writer(f)
-        writer.writerow(header)
+                # Send request and parse the tables in response
+                response = requests.get("https://" + urllib.parse.quote(url), headers=headers)
 
-        count_e = 0  # error count
-        for index, row in df.iterrows():
-            synonyms = ""
-            formula = ""
-            weight = ""
-            density = ""
-            water_solubility = ""
+                soup = BeautifulSoup(response.text, 'html5lib')
+                tables = soup.find_all('table')
 
-            chemical_name = row['Chemical Name'].title().replace("/", " ")
-            
-            headers = {
-                'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:107.0) Gecko/20100101 Firefox/107.0',
-            }
-            url = "www.chembk.com/en/chem/" + chemical_name
+                try:
+                    ## summary table
+                    rows = tables[0].find_all('tr')
+                    for row in rows:
+                        tds = row.find_all('td')
 
-            response = requests.get("https://" + urllib.parse.quote(url), headers=headers)
+                        # Extract Synonyms into an array
+                        if tds[0].text == 'Synonyms':
+                            synonyms = [a.text for a in tds[1].find_all('a')]
+                            
 
-            soup = BeautifulSoup(response.text, 'html5lib')
-            tables = soup.find_all('table')
-
-            try:
-                ## summary
-                rows = tables[0].find_all('tr')
-                for row in rows:
-                    tds = row.find_all('td')
-
-                    # Extract Synonyms into an array
-                    if tds[0].text == 'Synonyms':
-                        synonyms = [a.text for a in tds[1].find_all('a')]
+                    ## infos table
+                    rows = tables[1].find_all('tr')
+                    for row in rows:
+                        tds = row.find_all('td')
                         
+                        # Extract formula
+                        if tds[0].text == 'Molecular Formula':
+                            formula = tds[1].text
 
-                ## infos
-                rows = tables[1].find_all('tr')
-                for row in rows:
-                    tds = row.find_all('td')
+                        # Extract molecular weight
+                        if tds[0].text == 'Molar Mass':
+                            weight = tds[1].text
+                            
+                        # Extract Density
+                        if tds[0].text == "Density":
+                            density = tds[1].text
+
+                        # Extract Water Solubility
+                        if tds[0].text == "Water Solubility":
+                            water_solubility = tds[1].text
                     
-                    # Extract formula
-                    if tds[0].text == 'Molecular Formula':
-                        formula = tds[1].text
+                    print(chemical_name, "Completed")
 
-                    # Extract molecular weight
-                    if tds[0].text == 'Molar Mass':
-                        weight = tds[1].text
-                        
-                    # Extract Density
-                    if tds[0].text == "Density":
-                        density = tds[1].text
+                    # Write result to csv file
+                    writer.writerow([index, chemical_name, "", water_solubility,
+                                    weight, density, formula, "", synonyms, ""])
 
-                    # Extract Water Solubility
-                    if tds[0].text == "Water Solubility":
-                        water_solubility = tds[1].text
-                
-                print(chemical_name, "Completed")
+                except Exception as e:
+                    
+                    # Write only the index and chemical name if no results found / if errors occured.
+                    writer.writerow(
+                        [index, chemical_name, "", "", "", "", "", "", ""])
+                    count_e += 1
+                    print("error" + str(count_e) + ": " +
+                        chemical_name + " - " + str(e))
+                    continue
 
-                # Write result to csv file
-                writer.writerow([index, chemical_name, "", water_solubility,
-                                weight, density, formula, "", synonyms, ""])
+        except Exception as e:
+            print(e)
 
-            except Exception as e:
-                
-                # Write only the index and chemical name if no results found / if errors occured.
-                writer.writerow(
-                    [index, chemical_name, "", "", "", "", "", "", ""])
-                count_e += 1
-                print("error" + str(count_e) + ": " +
-                      chemical_name + " - " + str(e))
-                continue
 
-    except Exception as e:
-        print(e)
+
+if __name__ == '__main__':
+    
+    # Read clean chemical list into dataframe
+    df = pd.read_excel("cleansingPipeline/Property_and_functionalities_List.xlsx", header = 0)
+    print(len(df))
+
+    # Drop duplicate chemical names if needed
+    # df = df.drop_duplicates(subset='Chemical Name', keep="last")
+    # print(len(df))
+    # df = df.iloc[:1600]
+
+    # Set file header
+    header = ['id', 'Chemical Name', 'LogP (Partition coefficient)', 'LogS (water solubility of the ingredient)',
+            'Molecular weight', 'Density', 'Chemical formula', 'Structure', 'Synonyms', 'Functionalities']
+
+    scrapChemBK(header = header, df = df, outputFileName = 'ScrapingResults/Properties_Chembk_new.csv')
